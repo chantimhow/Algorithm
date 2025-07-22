8d16fa19f1599b90aca54698550bf44e7c1c1041
chantimhow <pixelsgt@gmail.com>
2025-07-22 16:37:17 +0800

    nothin much

--- LAB_4/app.cpp
+++ LAB_4/app.cpp
@@ -1,14 +1,23 @@
 #include	<iostream>
 #include    <iomanip>
 #include	"List.h"
+#include 	"List.cpp"
+#include "Node.cpp"
+
 //#include    "Person.h"
 
 using namespace std;
 
-bool printList(List);
+bool printList(List list);
 
 int main() 
 {
+	List list1;
+	list1.insert(1,3);
+	list1.insert(2,6);
+	list1.insert(3,2);
+	list1.insert(4,9);
+	list1.insert(5,7);
 	
 	
 	
@@ -25,8 +34,13 @@ int main()
 	
 
 	
-	system("pause");
+	
 	return 0;
-}
 
+	
+}
+bool printList(List list){
+	
+
+}
 
